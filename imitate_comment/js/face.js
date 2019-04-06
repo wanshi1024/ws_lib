77dfed435ea45957673bc02aecf7b427b2431936
ws10204 <2942504702@qq.com>
2019-04-06 18:06:19 +0800

    ws

---
+++ imitate_comment/js/face.js
@@ -0,0 +1,281 @@
+const faces = [{
+        s: 'images/smilea_thumb.gif',
+        title: '呵呵'
+    },
+    {
+        s: 'images/tootha_thumb.gif',
+        title: '嘻嘻'
+    },
+    {
+        s: 'images/laugh.gif',
+        title: '哈哈'
+    },
+    {
+        s: 'images/tza_thumb.gif',
+        title: '可爱'
+    },
+    {
+        s: 'images/kl_thumb.gif',
+        title: '可怜'
+    },
+    {
+        s: 'images/kbsa_thumb.gif',
+        title: '挖鼻屎'
+    },
+    {
+        s: 'images/cj_thumb.gif',
+        title: '吃惊'
+    },
+    {
+        s: 'images/shamea_thumb.gif',
+        title: '害羞'
+    },
+    {
+        s: 'images/zy_thumb.gif',
+        title: '挤眼'
+    },
+    {
+        s: 'images/bs2_thumb.gif',
+        title: '鄙视'
+    },
+    {
+        s: 'images/lovea_thumb.gif',
+        title: '爱你'
+    },
+    {
+        s: 'images/sada_thumb.gif',
+        title: '泪'
+    },
+    {
+        s: 'images/heia_thumb.gif',
+        title: '偷笑'
+    },
+    {
+        s: 'images/qq_thumb.gif',
+        title: '亲亲'
+    },
+    {
+        s: 'images/sb_thumb.gif',
+        title: '生病'
+    },
+    {
+        s: 'images/mb_thumb.gif',
+        title: '太开心'
+    },
+    {
+        s: 'images/ldln_thumb.gif',
+        title: '懒得理你'
+    },
+    {
+        s: 'images/lovea_thumb.gif',
+        title: '爱你'
+    },
+    {
+        s: 'images/yhh_thumb.gif',
+        title: '左哼哼'
+    },
+    {
+        s: 'images/zhh_thumb.gif',
+        title: '右哼哼'
+    },
+    {
+        s: 'images/x_thumb.gif',
+        title: '嘘'
+    },
+    {
+        s: 'images/cry.gif',
+        title: '衰'
+    },
+    {
+        s: 'images/wq_thumb.gif',
+        title: '委屈'
+    },
+    {
+        s: 'images/t_thumb.gif',
+        title: '吐'
+    },
+    {
+        s: 'images/k_thumb.gif',
+        title: '打哈气'
+    },
+    {
+        s: 'images/bba_thumb.gif',
+        title: '抱抱'
+    },
+    {
+        s: 'images/angrya_thumb.gif',
+        title: '怒'
+    },
+    {
+        s: 'images/yw_thumb.gif',
+        title: '疑问'
+    },
+    {
+        s: 'images/cza_thumb.gif',
+        title: '馋嘴'
+    },
+    {
+        s: 'images/88_thumb.gif',
+        title: '拜拜'
+    },
+    {
+        s: 'images/sk_thumb.gif',
+        title: '思考'
+    },
+    {
+        s: 'images/sweata_thumb.gif',
+        title: '汗'
+    },
+    {
+        s: 'images/sleepya_thumb.gif',
+        title: '困'
+    },
+    {
+        s: 'images/sleepa_thumb.gif',
+        title: '睡觉'
+    },
+    {
+        s: 'images/money_thumb.gif',
+        title: '钱'
+    },
+    {
+        s: 'images/sw_thumb.gif',
+        title: '失望'
+    },
+    {
+        s: 'images/cry.gif',
+        title: '酷'
+    },
+    {
+        s: 'images/hsa_thumb.gif',
+        title: '花心'
+    },
+    {
+        s: 'images/hatea_thumb.gif',
+        title: '哼'
+    },
+    {
+        s: 'images/gza_thumb.gif',
+        title: '鼓掌'
+    },
+    {
+        s: 'images/dizzya_thumb.gif',
+        title: '晕'
+    },
+    {
+        s: 'images/bs_thumb.gif',
+        title: '悲伤'
+    },
+    {
+        s: 'images/crazya_thumb.gif',
+        title: '抓狂'
+    },
+    {
+        s: 'images/h_thumb.gif',
+        title: '黑线'
+    },
+    {
+        s: 'images/yx_thumb.gif',
+        title: '阴险'
+    },
+    {
+        s: 'images/nm_thumb.gif',
+        title: '怒骂'
+    },
+    {
+        s: 'images/hearta_thumb.gif',
+        title: '心'
+    },
+    {
+        s: 'images/unheart.gif',
+        title: '伤心'
+    },
+    {
+        s: 'images/pig.gif',
+        title: '猪头'
+    },
+    {
+        s: 'images/ok_thumb.gif',
+        title: 'OK'
+    },
+    {
+        s: 'images/ye_thumb.gif',
+        title: '耶'
+    },
+    {
+        s:'images/good_thumb.gif',
+        title:'good'
+    },
+    {
+        s: 'images/no_thumb.gif',
+        title: '不要'
+    },
+    {
+        s: 'images/z2_thumb.gif',
+        title: '赞'
+    },
+    {
+        s: 'images/come_thumb.gif',
+        title: '来'
+    },
+    {
+        s: 'images/sad_thumb.gif',
+        title: '弱'
+    },
+    {
+        s: 'images/lazu_thumb.gif',
+        title: '蜡烛'
+    },
+    {
+        s: 'images/clock_thumb.gif',
+        title: '钟'
+    },
+    {
+        s: 'images/cake.gif',
+        title: '蛋糕'
+    },
+    {
+        s: 'images/m_thumb.gif',
+        title: '话筒'
+    },
+    {
+        s: 'images/weijin_thumb.gif',
+        title: '围巾'
+    },
+    {
+        s: 'images/lxhzhuanfa_thumb.gif',
+        title: '转发'
+    },
+    {
+        s: 'images/lxhluguo_thumb.gif',
+        title: '路过这儿'
+    },
+    {
+        s: 'images/bofubianlian_thumb.gif',
+        title: '变脸'
+    },
+    {
+        s: 'images/gbzkun_thumb.gif',
+        title: '困'
+    },
+    {
+        s: 'images/boboshengmenqi_thumb.gif',
+        title: '生闷气'
+    },
+    {
+        s: 'images/chn_buyaoya_thumb.gif',
+        title: '不要啊'
+    },
+    {
+        s: 'images/daxiongleibenxiong_thumb.gif',
+        title: '泪奔'
+    },
+    {
+        s: 'images/cat_yunqizhong_thumb.gif',
+        title: '运气中'
+    },
+    {
+        s: 'images/youqian_thumb.gif',
+        title: '有钱'
+    }
+]
