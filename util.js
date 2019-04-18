ed66244ec076c6d9ffd326858e7b9c1bf477b820
wanshi1024 <2942504702.com>
2019-04-19 07:56:29 +0800

    ws

--- util.js
+++ util.js
@@ -1,14 +1,12 @@
 
 /**
- * 格式化时间
- * 
+ * 自定义格式化时间
  * @param  {time} 指定时间 || new Date() 
  * @param  {cFormat} 格式
  * @return {String} 字符串
- *
  * @example formatTime('2018-1-29', '{y}/{m}/{d} 星期{a} {h}:{i}:{s}') // -> 2018/01/29 星期四 00:00:00
  */
- function formatTime(time, cFormat) {
+function formatTime(time, cFormat) {
     if (arguments.length === 0) return null
     if ((time + '').length === 10) {
         time = +time * 1000
@@ -40,11 +38,494 @@
     return time_str
 }
 
+/** 
+ * 数字类型数组排序
+ * @param  {*} Array
+ * @param  {type} Number 1：从小到大 2:从大到小 3：随机 ps:不传type原状返回
+ * @return {Array} Array
+ */
+function sort(arr, type) {
+    return arr.sort((a, b) => {
+        switch (type) {
+            case 1:
+                return a - b;
+            case 2:
+                return b - a;
+            case 3:
+                return Math.random() - 0.5;
+            default:
+                return arr;
+        }
+    })
+}
 
+/**
+ * 数组去重
+ * @param {*} Array 
+ * @returns {*} Array
+ */
+function removeRepeat(arr) {
+    return [...new Set(arr)];
+}
+
+/**
+ * 删除数组中指定元素
+ * @param {*} Array 
+ * @param {*} any 
+ * @param {*} boolean 是否删除所有 默认false 
+ * @returns {*} Array
+ */
+function removeElement(arr, element, boolean = false) {
+    if (boolean) {
+        while (true) {
+            var index = arr.indexOf(element);
+            if (index === -1) break;
+            if (index > -1) {
+                arr.splice(index, 1);
+            }
+        }
+    } else {
+        var index = arr.indexOf(element);
+        if (index > -1) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
+}
+
+/**
+ * 字符串去除空格
+ * @param  {str}
+ * @param  {type} type:  1-所有空格(default)  2-前后空格  3-前空格 4-后空格
+ * @return {String}
+ */
+function trim(str, type) {
+    type = type || 1
+    switch (type) {
+        case 1:
+            return str.replace(/\s+/g, "");
+        case 2:
+            return str.replace(/(^\s*)|(\s*$)/g, "");
+        case 3:
+            return str.replace(/(^\s*)/g, "");
+        case 4:
+            return str.replace(/(\s*$)/g, "");
+        default:
+            return str;
+    }
+}
+
+/**
+ * 字符串大小写转换
+ * @param  {str} str
+ * @param  {type} type:  1:首字母大写(defuse)  2：首页母小写  3：大小写互换  4：全部大写  5：全部小写
+ * @return {String}
+ */
+function changeCase(str, type) {
+    type = type || 3
+    switch (type) {
+        case 1:
+            return str.replace(/\b\w+\b/g, function (word) {
+                return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+
+            });
+        case 2:
+            return str.replace(/\b\w+\b/g, function (word) {
+                return word.substring(0, 1).toLowerCase() + word.substring(1).toUpperCase();
+            });
+        case 3:
+            return str.split('').map(function (word) {
+                if (/[a-z]/.test(word)) {
+                    return word.toUpperCase();
+                } else {
+                    return word.toLowerCase()
+                }
+            }).join('')
+        case 4:
+            return str.toUpperCase();
+        case 5:
+            return str.toLowerCase();
+        default:
+            return str;
+    }
+}
+/**
+ * 密码强度检验
+ * @param {*} str 
+ * @returns number
+ */
+function checkPwd(str) {
+    var Lv = 0;
+    if (str.length < 6) {
+        return Lv
+    }
+    if (/[0-9]/.test(str)) {
+        Lv++
+    }
+    if (/[a-z]/.test(str)) {
+        Lv++
+    }
+    if (/[A-Z]/.test(str)) {
+        Lv++
+    }
+    if (/[\.|-|_]/.test(str)) {
+        Lv++
+    }
+    return Lv;
+}
+
+/**
+ * 过滤html代码 过滤后的字符串方便在页面显示代码
+ * @param {*} str 
+ * @returns str
+ */
+function filterTag(str) {
+    str = str.replace(/&/ig, "&amp;");
+    str = str.replace(/</ig, "&lt;");
+    str = str.replace(/>/ig, "&gt;");
+    str = str.replace(" ", "&nbsp;");
+    return str;
+}
+
+/**
+ * 返回两数之间的随机数
+ * @param {*} min 
+ * @param {*} max 
+ * @returns number
+ */
+function random(min, max) {
+    if (arguments.length === 2) {
+        return Math.floor(min + Math.random() * ((max + 1) - min))
+    } else {
+        return null;
+    }
+}
+/**
+ * 将阿拉伯数字翻译成中文的大写数字
+ * @param {*} num 
+ * @returns str
+ */
+function numberToChinese(num) {
+    var AA = new Array("零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十");
+    var BB = new Array("", "十", "百", "仟", "萬", "億", "点", "");
+    var a = ("" + num).replace(/(^0*)/g, "").split("."),
+        k = 0,
+        re = "";
+    for (var i = a[0].length - 1; i >= 0; i--) {
+        switch (k) {
+            case 0:
+                re = BB[7] + re;
+                break;
+            case 4:
+                if (!new RegExp("0{4}//d{" + (a[0].length - i - 1) + "}$")
+                    .test(a[0]))
+                    re = BB[4] + re;
+                break;
+            case 8:
+                re = BB[5] + re;
+                BB[7] = BB[5];
+                k = 0;
+                break;
+        }
+        if (k % 4 == 2 && a[0].charAt(i + 2) != 0 && a[0].charAt(i + 1) == 0)
+            re = AA[0] + re;
+        if (a[0].charAt(i) != 0)
+            re = AA[a[0].charAt(i)] + BB[k % 4] + re;
+        k++;
+    }
+
+    if (a.length > 1) // 加上小数部分(如果有小数部分)
+    {
+        re += BB[6];
+        for (var i = 0; i < a[1].length; i++)
+            re += AA[a[1].charAt(i)];
+    }
+    if (re == '一十')
+        re = "十";
+    if (re.match(/^一/) && re.length == 3)
+        re = re.replace("一", "");
+    return re;
+}
+
+
+/**
+ * 将数字金额转换为大写金额
+ * @param {*} Num 
+ * @returns str
+ */
+function moneyNumToChinese(Num) {
+    //判断如果传递进来的不是字符的话转换为字符
+    if (typeof Num == "number") {
+        Num = new String(Num);
+    };
+    Num = Num.replace(/,/g, "") //替换tomoney()中的“,”
+    Num = Num.replace(/ /g, "") //替换tomoney()中的空格
+    Num = Num.replace(/￥/g, "") //替换掉可能出现的￥字符
+    if (isNaN(Num)) { //验证输入的字符是否为数字
+        //alert("请检查小写金额是否正确");
+        return "";
+    };
+    //字符处理完毕后开始转换，采用前后两部分分别转换
+    var part = String(Num).split(".");
+    var newchar = "";
+    //小数点前进行转化
+    for (var i = part[0].length - 1; i >= 0; i--) {
+        if (part[0].length > 10) {
+            return "";
+            //若数量超过拾亿单位，提示
+        }
+        var tmpnewchar = ""
+        var perchar = part[0].charAt(i);
+        switch (perchar) {
+            case "0":
+                tmpnewchar = "零" + tmpnewchar;
+                break;
+            case "1":
+                tmpnewchar = "壹" + tmpnewchar;
+                break;
+            case "2":
+                tmpnewchar = "贰" + tmpnewchar;
+                break;
+            case "3":
+                tmpnewchar = "叁" + tmpnewchar;
+                break;
+            case "4":
+                tmpnewchar = "肆" + tmpnewchar;
+                break;
+            case "5":
+                tmpnewchar = "伍" + tmpnewchar;
+                break;
+            case "6":
+                tmpnewchar = "陆" + tmpnewchar;
+                break;
+            case "7":
+                tmpnewchar = "柒" + tmpnewchar;
+                break;
+            case "8":
+                tmpnewchar = "捌" + tmpnewchar;
+                break;
+            case "9":
+                tmpnewchar = "玖" + tmpnewchar;
+                break;
+        }
+        switch (part[0].length - i - 1) {
+            case 0:
+                tmpnewchar = tmpnewchar + "元";
+                break;
+            case 1:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "拾";
+                break;
+            case 2:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "佰";
+                break;
+            case 3:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "仟";
+                break;
+            case 4:
+                tmpnewchar = tmpnewchar + "万";
+                break;
+            case 5:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "拾";
+                break;
+            case 6:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "佰";
+                break;
+            case 7:
+                if (perchar != 0) tmpnewchar = tmpnewchar + "仟";
+                break;
+            case 8:
+                tmpnewchar = tmpnewchar + "亿";
+                break;
+            case 9:
+                tmpnewchar = tmpnewchar + "拾";
+                break;
+        }
+        var newchar = tmpnewchar + newchar;
+    }
+    //小数点之后进行转化
+    if (Num.indexOf(".") != -1) {
+        if (part[1].length > 2) {
+            // alert("小数点之后只能保留两位,系统将自动截断");
+            part[1] = part[1].substr(0, 2)
+        }
+        for (i = 0; i < part[1].length; i++) {
+            tmpnewchar = ""
+            perchar = part[1].charAt(i)
+            switch (perchar) {
+                case "0":
+                    tmpnewchar = "零" + tmpnewchar;
+                    break;
+                case "1":
+                    tmpnewchar = "壹" + tmpnewchar;
+                    break;
+                case "2":
+                    tmpnewchar = "贰" + tmpnewchar;
+                    break;
+                case "3":
+                    tmpnewchar = "叁" + tmpnewchar;
+                    break;
+                case "4":
+                    tmpnewchar = "肆" + tmpnewchar;
+                    break;
+                case "5":
+                    tmpnewchar = "伍" + tmpnewchar;
+                    break;
+                case "6":
+                    tmpnewchar = "陆" + tmpnewchar;
+                    break;
+                case "7":
+                    tmpnewchar = "柒" + tmpnewchar;
+                    break;
+                case "8":
+                    tmpnewchar = "捌" + tmpnewchar;
+                    break;
+                case "9":
+                    tmpnewchar = "玖" + tmpnewchar;
+                    break;
+            }
+            if (i == 0) tmpnewchar = tmpnewchar + "角";
+            if (i == 1) tmpnewchar = tmpnewchar + "分";
+            newchar = newchar + tmpnewchar;
+        }
+    }
+    //替换所有无用汉字
+    while (newchar.search("零零") != -1)
+        newchar = newchar.replace("零零", "零");
+    newchar = newchar.replace("零亿", "亿");
+    newchar = newchar.replace("亿万", "亿");
+    newchar = newchar.replace("零万", "万");
+    newchar = newchar.replace("零元", "元");
+    newchar = newchar.replace("零角", "");
+    newchar = newchar.replace("零分", "");
+    if (newchar.charAt(newchar.length - 1) == "元") {
+        newchar = newchar + "整"
+    }
+    return newchar;
+}
+
+/**
+ * 操作浏览器数据仓库类
+ */
+class StorageFn {
+    constructor() {
+        this.ls = window.localStorage;
+        this.ss = window.sessionStorage;
+    }
+
+    /*-----------------cookie---------------------*/
+    /*设置cookie*/
+    setCookie(name, value, day) {
+        var setting = arguments[0];
+        if (Object.prototype.toString.call(setting).slice(8, -1) === 'Object') {
+            for (var i in setting) {
+                var oDate = new Date();
+                oDate.setDate(oDate.getDate() + day);
+                document.cookie = i + '=' + setting[i] + ';expires=' + oDate;
+            }
+        } else {
+            var oDate = new Date();
+            oDate.setDate(oDate.getDate() + day);
+            document.cookie = name + '=' + value + ';expires=' + oDate;
+        }
+
+    }
+
+    /*获取cookie*/
+    getCookie(name) {
+        var arr = document.cookie.split('; ');
+        for (var i = 0; i < arr.length; i++) {
+            var arr2 = arr[i].split('=');
+            if (arr2[0] == name) {
+                return arr2[1];
+            }
+        }
+        return '';
+    }
+
+    /*删除cookie*/
+    removeCookie(name) {
+        this.setCookie(name, 1, -1);
+    }
+
+
+    /*-----------------localStorage---------------------*/
+    /*设置localStorage*/
+    setLocal(key, val) {
+        var setting = arguments[0];
+        if (Object.prototype.toString.call(setting).slice(8, -1) === 'Object') {
+            for (var i in setting) {
+                this.ls.setItem(i, JSON.stringify(setting[i]))
+            }
+        } else {
+            this.ls.setItem(key, JSON.stringify(val))
+        }
+
+    }
+
+    /*获取localStorage*/
+    getLocal(key) {
+        if (key) return JSON.parse(this.ls.getItem(key))
+        return null;
+
+    }
+
+    /*移除localStorage*/
+    removeLocal(key) {
+        this.ls.removeItem(key)
+    }
+
+    /*移除所有localStorage*/
+    clearLocal() {
+        this.ls.clear()
+    }
+
+
+    /*-----------------sessionStorage---------------------*/
+    /*设置sessionStorage*/
+    setSession(key, val) {
+        var setting = arguments[0];
+        if (Object.prototype.toString.call(setting).slice(8, -1) === 'Object') {
+            for (var i in setting) {
+                this.ss.setItem(i, JSON.stringify(setting[i]))
+            }
+        } else {
+            this.ss.setItem(key, JSON.stringify(val))
+        }
+
+    }
+
+    /*获取sessionStorage*/
+    getSession(key) {
+        if (key) return JSON.parse(this.ss.getItem(key))
+        return null;
+
+    }
+
+    /*移除sessionStorage*/
+    removeSession(key) {
+        this.ss.removeItem(key)
+    }
+
+    /*移除所有sessionStorage*/
+    clearSession() {
+        this.ss.clear()
+    }
+}
 
 // 将封装的函数作为对象导出
-const util = {
+const Util = {
     formatTime,
-    
+    sort,
+    removeRepeat,
+    removeElement,
+    trim,
+    changeCase,
+    checkPwd,
+    filterTag,
+    random,
+    numberToChinese,
+    moneyNumToChinese,
+    StorageFn
 }
-module.exports = util;
+module.exports = Util;
+// export default util
